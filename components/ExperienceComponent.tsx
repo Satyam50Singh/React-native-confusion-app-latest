80e256749ed04e0b39d0ca2eab3984c5bef9a2e3
Satyam Singh <satyam.singh@tvsmotor.com>
2024-09-23 08:11:00 +0530

    Designed custom radio buttons using TouchableOpacity

--- components/ExperienceComponent.tsx
+++ components/ExperienceComponent.tsx
@@ -1,31 +1,139 @@
-import React from 'react';
-import {Text, StyleSheet, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+  TextInput,
+} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 function ExperienceComponent() {
+  const [isYesSelected, setIsYesSelected] = useState(false);
+  const [isNoSelected, setIsNoSelected] = useState(false);
+
   return (
-    <View style={{padding: 16, flexDirection: 'row', alignItems: 'center'}}>
-      <TouchableOpacity
-        onPress={() => console.warn('Button Clicked')}
-        activeOpacity={0.7}>
-        <View style={styles.buttonContainer}>
-          <MaterialIcons
-            name="arrow-back-ios-new"
-            size={24}
-            color="white"
-            style={{padding: 4}}
+    <View style={{flex: 1}}>
+      <View style={{padding: 16, flexDirection: 'row', alignItems: 'center'}}>
+        <TouchableOpacity
+          onPress={() => console.warn('Button Clicked')}
+          activeOpacity={0.7}>
+          <View style={styles.buttonContainer}>
+            <MaterialIcons
+              name="arrow-back-ios-new"
+              size={24}
+              color="white"
+              style={{padding: 4}}
+            />
+          </View>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 22,
+            textAlign: 'center',
+            margin: 16,
+            fontWeight: 600,
+          }}>
+          Select Type
+        </Text>
+      </View>
+
+      <Text style={{padding: 16, fontSize: 20, color: 'black'}}>
+        Are you a corporate Customer ?
+      </Text>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{padding: 16}}>
+          <TouchableHighlight
+            underlayColor="transparent"
+            onPress={() => setIsYesSelected(!isYesSelected)}>
+            <View
+              style={{
+                borderRadius: 12,
+                borderColor: 'grey',
+                borderWidth: 2,
+                paddingVertical: 16,
+                alignItems: 'center',
+                paddingHorizontal: 20,
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  // padding: 2,
+                  borderColor: 'black',
+                  borderWidth: 2,
+                  width: 22,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: 22,
+                  borderRadius: 25,
+                  // backgroundColor: 'red',
+                  marginRight: 8,
+                }}>
+                {isYesSelected ? (
+                  <View
+                    style={{
+                      width: 11,
+                      height: 11,
+                      backgroundColor: 'black',
+                      borderRadius: 25,
+                    }}></View>
+                ) : null}
+              </View>
+              <Text style={{fontSize: 18, color: 'black'}}>Yes</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+        <View style={{padding: 16}}>
+          <TouchableHighlight
+            underlayColor="transparent"
+            onPress={() => setIsNoSelected(!isNoSelected)}>
+            <View
+              style={{
+                borderRadius: 12,
+                borderColor: 'grey',
+                borderWidth: 2,
+                paddingVertical: 16,
+                alignItems: 'center',
+                paddingHorizontal: 20,
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  // padding: 2,
+                  borderColor: 'black',
+                  borderWidth: 2,
+                  width: 22,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: 22,
+                  borderRadius: 25,
+                  // backgroundColor: 'red',
+                  marginRight: 8,
+                }}>
+                {isNoSelected ? (
+                  <View
+                    style={{
+                      width: 11,
+                      height: 11,
+                      backgroundColor: 'black',
+                      borderRadius: 25,
+                    }}></View>
+                ) : null}
+              </View>
+              <Text style={{fontSize: 18, color: 'black'}}>No</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </View>
+      {isNoSelected ? (
+        <View style={{paddingHorizontal: 16, paddingVertical: 3}}>
+          <TextInput
+            style={styles.textInput}
+            placeholder={'Select Account Type'}
           />
         </View>
-      </TouchableOpacity>
-      <Text
-        style={{
-          fontSize: 22,
-          textAlign: 'center',
-          margin: 16,
-          fontWeight: 600,
-        }}>
-        Select Type
-      </Text>
+      ) : null}
     </View>
   );
 }
@@ -42,6 +150,12 @@ const styles = StyleSheet.create({
     marginTop: 12,
     textAlign: 'center',
     backgroundColor: 'blue',
+  },
+  textInput: {
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: 'purple',
+    marginBottom: 8,
   },
 });
 
